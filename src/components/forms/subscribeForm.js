80990c20f9a047c70bf96cae1b90ba08622db79d
Marsal Jean-Baptiste <jmarsal@student.42.fr>
2017-07-10 17:31:32 +0200

    Mise en forme de subscribe

--- src/components/forms/subscribeForm.js
+++ src/components/forms/subscribeForm.js
@@ -13,6 +13,7 @@ import {
 	Panel,
 	FormControl,
 	FormGroup,
+	Form,
 	ControlLabel,
 	ButtonToolbar,
 	Button
@@ -95,139 +96,109 @@ class SubscribeForm extends React.Component {
 		const { showModal } = this.props;
 
 		return (
-			<Row>
-				<Col sm={10}>
-					{this.props.errorsUsers
-						? this.props.errorsUsers.map((errorsArr, i) => {
-								return (
-									<Alert key={i} bsStyle="warning">
-										{errorsArr.msg}
-									</Alert>
-								);
-							})
-						: ''}
-					{this.props.errorsUpload
-						? this.props.errorsUpload.map((errorsArr, i) => {
-								return (
-									<Alert key={i} bsStyle="warning">
-										{errorsArr.msg}
-									</Alert>
-								);
-							})
-						: ''}
-					{this.state.sizeError
-						? <Alert key="sizeError" bsStyle="warning">
-								{this.state.sizeError}
-							</Alert>
-						: ''}
-					<OverlayTrigger
-						ref="emptyLogin"
-						placement="right"
-						overlay={
-							<Popover id="popover-positioned-right">
-								Please enter your login.
-							</Popover>
-						}
-					>
-						<FormGroup controlId="username" validationState={this.props.validation}>
-							<ControlLabel>Login</ControlLabel>
-							<FormControl type="text" placeholder="Enter your login" ref="username" />
-							<FormControl.Feedback />
-						</FormGroup>
-					</OverlayTrigger>
+			<Form horizontal>
+				<FormGroup>
+					<Col sm={10}>
+						{this.props.errorsUsers
+							? this.props.errorsUsers.map((errorsArr, i) => {
+									return (
+										<Alert key={i} bsStyle="warning">
+											{errorsArr.msg}
+										</Alert>
+									);
+								})
+							: ''}
+						{this.props.errorsUpload
+							? this.props.errorsUpload.map((errorsArr, i) => {
+									return (
+										<Alert key={i} bsStyle="warning">
+											{errorsArr.msg}
+										</Alert>
+									);
+								})
+							: ''}
+						{this.state.sizeError
+							? <Alert key="sizeError" bsStyle="warning">
+									{this.state.sizeError}
+								</Alert>
+							: ''}
+					</Col>
+				</FormGroup>
+				<FormGroup controlId="username" validationState={this.props.validation}>
+					<Col componentClass={ControlLabel} sm={2}>
+						Login
+					</Col>
+					<Col sm={10}>
+						<FormControl type="text" placeholder="Enter your login" ref="username" />
+						<FormControl.Feedback />
+					</Col>
+				</FormGroup>
 
-					<OverlayTrigger
-						ref="emptyFirstname"
-						placement="right"
-						overlay={
-							<Popover id="popover-positioned-right">
-								Please enter your firstname.
-							</Popover>
-						}
-					>
-						<FormGroup controlId="firstname" validationState={this.props.validation}>
-							<ControlLabel>Firstname</ControlLabel>
-							<FormControl type="text" placeholder="Enter your firstname" ref="firstname" />
-							<FormControl.Feedback />
-						</FormGroup>
-					</OverlayTrigger>
+				<FormGroup controlId="firstname" validationState={this.props.validation}>
+					<Col componentClass={ControlLabel} sm={2}>
+						Firstname
+					</Col>
+					<Col sm={10}>
+						<FormControl type="text" placeholder="Enter your firstname" ref="firstname" />
+						<FormControl.Feedback />
+					</Col>
+				</FormGroup>
 
-					<OverlayTrigger
-						ref="emptyLastname"
-						placement="right"
-						overlay={
-							<Popover id="popover-positioned-right">
-								Please enter your lastname.
-							</Popover>
-						}
-					>
-						<FormGroup controlId="lastname" validationState={this.props.validation}>
-							<ControlLabel>Lastname</ControlLabel>
-							<FormControl type="text" placeholder="Enter your lastname" ref="lastname" />
-							<FormControl.Feedback />
-						</FormGroup>
-					</OverlayTrigger>
+				<FormGroup controlId="lastname" validationState={this.props.validation}>
+					<Col componentClass={ControlLabel} sm={2}>
+						Lastname
+					</Col>
+					<Col sm={10}>
+						<FormControl type="text" placeholder="Enter your lastname" ref="lastname" />
+						<FormControl.Feedback />
+					</Col>
+				</FormGroup>
 
-					<OverlayTrigger
-						ref="emptyEmail"
-						placement="right"
-						overlay={
-							<Popover id="popover-positioned-right">
-								Please enter your email.
-							</Popover>
-						}
-					>
-						<FormGroup controlId="email" validationState={this.props.validation}>
-							<ControlLabel>Email</ControlLabel>
-							<FormControl type="email" placeholder="Enter your email" ref="email" />
-							<FormControl.Feedback />
-						</FormGroup>
-					</OverlayTrigger>
+				<FormGroup controlId="email" validationState={this.props.validation}>
+					<Col componentClass={ControlLabel} sm={2}>
+						Email
+					</Col>
+					<Col sm={10}>
+						<FormControl type="email" placeholder="Enter your email" ref="email" />
+						<FormControl.Feedback />
+					</Col>
+				</FormGroup>
 
-					<OverlayTrigger
-						ref="emptyPassword"
-						placement="right"
-						overlay={
-							<Popover id="popover-positioned-right">
-								Please enter your password.
-							</Popover>
-						}
-					>
-						<FormGroup controlId="password" validationState={this.props.validation}>
-							<ControlLabel>Password</ControlLabel>
-							<FormControl type="password" placeholder="Enter your password" ref="password" />
-							<FormControl.Feedback />
-						</FormGroup>
-					</OverlayTrigger>
+				<FormGroup controlId="password" validationState={this.props.validation}>
+					<Col componentClass={ControlLabel} sm={2}>
+						Password
+					</Col>
+					<Col sm={10}>
+						<FormControl type="password" placeholder="Enter your password" ref="password" />
+						<FormControl.Feedback />
+					</Col>
+				</FormGroup>
 
-					<OverlayTrigger
-						ref="emptyAvatar"
-						placement="right"
-						overlay={
-							<Popover id="popover-positioned-right">
-								Please choose a file.
-							</Popover>
-						}
-					>
-						<FormGroup controlId="avatar">
-							<ControlLabel>Avatar</ControlLabel>
-							<input type="file" ref={(ref) => (this.fileUpload = ref)} />
-						</FormGroup>
-					</OverlayTrigger>
+				<FormGroup controlId="avatar">
+					<Col componentClass={ControlLabel} sm={2}>
+						Avatar
+					</Col>
+					<Col sm={10}>
+						<input type="file" ref={(ref) => (this.fileUpload = ref)} />
+					</Col>
+				</FormGroup>
 
-					<ButtonToolbar>
-						<Button type="button" onClick={() => showModal(false)}>
-							Close
-						</Button>
-						<Button
-							onClick={!this.props.msg ? this.handleSubmit.bind(this) : this.resetForm.bind(this)}
-							bsStyle={!this.props.style ? 'primary' : this.props.style}
-						>
-							{!this.props.msg ? 'Sign in' : this.props.msg}
-						</Button>
-					</ButtonToolbar>
-				</Col>
-			</Row>
+				<FormGroup>
+					<Col sm={10}>
+						<ButtonToolbar>
+							<Button type="button" onClick={() => showModal(false)}>
+								Close
+							</Button>
+							<Button
+								onClick={!this.props.msg ? this.handleSubmit.bind(this) : this.resetForm.bind(this)}
+								bsStyle={!this.props.style ? 'primary' : this.props.style}
+							>
+								{!this.props.msg ? 'Sign in' : this.props.msg}
+							</Button>
+						</ButtonToolbar>
+					</Col>
+				</FormGroup>
+			</Form>
 		);
 	}
 }
